f64b35002ea480a92a7ba90a7c3e342d4853db18
AndreLuiz-JService <andre.oliveira.jservice@gmail.com>
2025-03-30 16:19:02 -0300

    Iniciando arquivo de cores da aplicaçao

--- lib/src/core/theme/app_colors.dart
+++ lib/src/core/theme/app_colors.dart
@@ -1,3 +1,17 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
   AppColors._();
+
+  // Primary Colors
+  static const Color primary = Color(0xFF232F69);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF37404E);
+  static const Color textSecondary = Color(0xFF5E646E);
+  static Color textTertiary = Color.fromRGBO(134, 140, 152, 0.75); // 75% opaco
+
+  // background colors
+  static const Color scaffoldBackground = Color(0xFFF4F5F6);
 }
